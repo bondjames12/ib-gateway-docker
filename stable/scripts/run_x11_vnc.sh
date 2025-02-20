0fecab7177a9e4e48f103ee907b38fe9e3cf3958
Colin Weber <colinweber@gmail.com>
2025-02-20 13:05:34 -0700

    Add expermiental rustdesk support (not working)

--- stable/scripts/run_x11_vnc.sh
+++ stable/scripts/run_x11_vnc.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
 
 x11vnc -ncache_cr -display :1 -forever -shared -logappend /var/log/x11vnc.log -bg -noipv6 -passwd "$VNC_SERVER_PASSWORD"
+
+mkdir -p /root/.config/rustdesk
+
+# Start RustDesk with custom server configuration and password
+#doesn't work?
+rustdesk \
+  --rendezvous-server $RUSTDESK_SERVER \
+  --relay-server $RUSTDESK_SERVER \
+  --key $RUSTDESK_KEY \
+  --password "$VNC_SERVER_PASSWORD" 
+  --option allow-linux-headless Y &
+
+  #Config Persistence: The ID and other settings are stored in /root/.config/rustdesk/RustDesk.toml
+  #If the container restarts without a persistent volume, the ID may regenerate
